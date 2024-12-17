e0ed0f9e8f33b6ab89c440ae5c7636e1f3d5f71d
Di <scharf.diana@googlemail.com>
2024-12-17 15:37:01 +0100

    Add Herd support (#36)
    * Detect Herd config if installed and use linked sites / certificates from there
    
    * Added verbose logging

--- app/Commands/ShareCurrentWorkingDirectoryCommand.php
+++ app/Commands/ShareCurrentWorkingDirectoryCommand.php
@@ -2,32 +2,83 @@
 
 namespace Expose\Client\Commands;
 
+use Symfony\Component\Console\Output\OutputInterface;
+use function Expose\Common\info;
+
 class ShareCurrentWorkingDirectoryCommand extends ShareCommand
 {
     protected $signature = 'share-cwd {host?} {--subdomain=} {--auth=} {--basicAuth=} {--dns=} {--domain=} {--qr} {--qr-code}';
 
+    protected ?string $configPath = null;
+
+    protected ?array $valetConfig = null;
+
     public function handle()
     {
+        $this->loadConfigurationFiles();
+
         $folderName = $this->detectName();
-        $host = $this->prepareSharedHost($folderName.'.'.$this->detectTld());
+
+        $host = $this->prepareSharedHost($folderName . '.' . $this->detectTld());
 
         $this->input->setArgument('host', $host);
 
-        if (! $this->option('subdomain')) {
+        if (!$this->option('subdomain')) {
             $this->input->setOption('subdomain', str_replace('.', '-', $folderName));
         }
 
         parent::handle();
     }
 
+    protected function loadConfigurationFiles(): void
+    {
+        $this->configPath = $this->getHerdValetConfigFilePath();
+
+        if ($this->configPath === null) {
+            $this->configPath = ($_SERVER['HOME'] ?? $_SERVER['USERPROFILE']) . DIRECTORY_SEPARATOR . '.config' . DIRECTORY_SEPARATOR . 'valet';
+        }
+
+        info("Detected Valet config path: " . $this->configPath, OutputInterface::VERBOSITY_VERBOSE);
+
+        if (file_exists($this->configFilePath())) {
+            $this->valetConfig = json_decode(file_get_contents($this->configFilePath()), true);
+
+            info("Trying to read Valet config file...", OutputInterface::VERBOSITY_VERBOSE);
+            info("JSON errors: " . json_last_error_msg(), OutputInterface::VERBOSITY_VERBOSE);
+        }
+    }
+
+    protected function getHerdValetConfigFilePath(): ?string
+    {
+        if (strpos(php_uname('s'), 'Windows') !== false) {
+            $isWindows = true;
+        } else {
+            $isWindows = false;
+        }
+
+        $configPath = null;
+
+        if (array_key_exists('HOME', $_SERVER)) {
+            if ($isWindows) {
+                $configPath = $_SERVER['HOME'] . '\.config\herd\config\valet';
+            } else {
+                $configPath = $_SERVER['HOME'] . '/Library/Application Support/Herd/config/valet';
+            }
+        } else if (array_key_exists('HOMEPATH', $_SERVER)) {
+            $configPath = $_SERVER['HOMEPATH'] . '\.config\herd\config\valet';
+        }
+
+        if (!file_exists($configPath)) {
+            $configPath = null;
+        }
+
+        return $configPath;
+    }
+
     protected function detectTld(): string
     {
-        $valetConfigFile = ($_SERVER['HOME'] ?? $_SERVER['USERPROFILE']).DIRECTORY_SEPARATOR.'.config'.DIRECTORY_SEPARATOR.'valet'.DIRECTORY_SEPARATOR.'config.json';
-
-        if (file_exists($valetConfigFile)) {
-            $valetConfig = json_decode(file_get_contents($valetConfigFile));
-
-            return $valetConfig->tld;
+        if($this->valetConfig && array_key_exists('tld', $this->valetConfig)) {
+            return $this->valetConfig['tld'];
         }
 
         return config('expose.default_tld', 'test');
@@ -36,23 +87,29 @@ class ShareCurrentWorkingDirectoryCommand extends ShareCommand
     protected function detectName(): string
     {
         $projectPath = getcwd();
-        $valetSitesPath = ($_SERVER['HOME'] ?? $_SERVER['USERPROFILE']).DIRECTORY_SEPARATOR.'.config'.DIRECTORY_SEPARATOR.'valet'.DIRECTORY_SEPARATOR.'Sites';
 
-        if (is_dir($valetSitesPath)) {
-            $site = collect(scandir($valetSitesPath))
-            ->skip(2)
-            ->map(function ($site) use ($valetSitesPath) {
-                return $valetSitesPath.DIRECTORY_SEPARATOR.$site;
-            })->mapWithKeys(function ($site) {
-                return [$site => readlink($site)];
-            })->filter(function ($sourcePath) use ($projectPath) {
-                return $sourcePath === $projectPath;
-            })
-            ->keys()
-            ->first();
+        info("Detecting name for $projectPath...", OutputInterface::VERBOSITY_VERBOSE);
+        info("Checking sites at " . $this->sitesPath(), OutputInterface::VERBOSITY_VERBOSE);
+
+        if (is_dir($this->sitesPath())) {
+            $site = collect(scandir($this->sitesPath()))
+                ->filter(function ($site) {
+                    return !in_array($site, ['.', '..', '.DS_Store']);
+                })
+                ->map(function ($site) {
+                    return $this->sitesPath() . DIRECTORY_SEPARATOR . $site;
+                })->mapWithKeys(function ($site) {
+                    return [$site => readlink($site)];
+                })->filter(function ($sourcePath) use ($projectPath) {
+                    return $sourcePath === $projectPath;
+                })
+                ->keys()
+                ->first();
 
             if ($site) {
                 $projectPath = $site;
+                info("Found linked site $site", OutputInterface::VERBOSITY_VERBOSE);
+
             }
         }
 
@@ -61,9 +118,7 @@ class ShareCurrentWorkingDirectoryCommand extends ShareCommand
 
     protected function detectProtocol($host): string
     {
-        $certificateFile = ($_SERVER['HOME'] ?? $_SERVER['USERPROFILE']).DIRECTORY_SEPARATOR.'.config'.DIRECTORY_SEPARATOR.'valet'.DIRECTORY_SEPARATOR.'Certificates'.DIRECTORY_SEPARATOR.$host.'.crt';
-
-        if (file_exists($certificateFile)) {
+        if (file_exists($this->certificateFile($host))) {
             return 'https://';
         }
 
@@ -72,6 +127,18 @@ class ShareCurrentWorkingDirectoryCommand extends ShareCommand
 
     protected function prepareSharedHost($host): string
     {
-        return $this->detectProtocol($host).$host;
+        return $this->detectProtocol($host) . $host;
+    }
+
+    protected function configFilePath(): string {
+        return $this->configPath . DIRECTORY_SEPARATOR . 'config.json';
+    }
+
+    protected function certificateFile(string $host): string {
+        return $this->configPath . DIRECTORY_SEPARATOR . 'Certificates' . DIRECTORY_SEPARATOR . $host . '.crt';
+    }
+
+    protected function sitesPath(): string {
+        return $this->configPath . DIRECTORY_SEPARATOR . 'Sites';
     }
 }
