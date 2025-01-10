9b03e0f4666aa0d310c9b6297a2964607a88b942
Di <scharf.diana@googlemail.com>
2025-01-10 13:06:15 +0100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use Expose\Client\Contracts\LogStorageContract;
 use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('database.default', 'sqlite');
+        config()->set('database.connections.sqlite.database', ':memory:');
+
+        $this->artisan('migrate')->run();
+        dump("set up");
+        dump(config('database.default'), config('database.connections.sqlite.database'));
+
+        app(LogStorageContract::class);
+
+    }
 }
