d42ece3078ef0768ca812d0d1ace1bbe3e38d697
Marcel Pociot <m.pociot@gmail.com>
2025-01-20 16:37:03 +0100

    Add catch-all command

---
+++ app/Commands/ShareCatchAllCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Expose\Client\Commands;
+
+
+use Expose\Client\Factory;
+use chillerlan\QRCode\Common\Version;
+use chillerlan\QRCode\Data\QRMatrix;
+use chillerlan\QRCode\Output\QROutputInterface;
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
+use Illuminate\Support\Str;
+use Psr\Http\Message\ServerRequestInterface;
+use React\EventLoop\Loop;
+use React\EventLoop\LoopInterface;
+use React\Http\HttpServer;
+use React\Http\Message\Response;
+use React\Socket\SocketServer;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use function Expose\Common\banner;
+use function Expose\Common\error;
+use function Expose\Common\info;
+use function Termwind\render;
+
+class ShareCatchAllCommand extends ShareCommand
+{
+
+
+    protected $signature = 'catch-all {host?} {--subdomain=} {--auth=} {--basicAuth=} {--dns=} {--domain=} {--qr} {--qr-code}';
+
+    protected $description = 'Share a catch-all site';
+
+    protected ?bool $isWindows = null;
+
+    public function handle()
+    {
+        $this->input->setArgument('host', $this->startCatchAllServer());
+
+        parent::handle();
+    }
+
+    protected function startCatchAllServer(): string
+    {
+        $http = new HttpServer(function (ServerRequestInterface $request) {
+            return Response::plaintext(
+                "OK"
+            );
+        });
+
+        $socket = new SocketServer('127.0.0.1:0');
+        $http->listen($socket);
+
+        $url = str_replace('tcp:', 'http:', $socket->getAddress());
+
+        info("Catch-All Server is listening on http://{$url}", options: OutputInterface::VERBOSITY_VERBOSE);
+
+        return $url;
+    }
+}
