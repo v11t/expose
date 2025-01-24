28a876a09c37ffe7cbd1f8ada5d1b78a350dfc19
Diana Scharf <scharf.diana@googlemail.com>
2025-01-24 15:04:28 +0100

    More tests!

--- tests/Feature/Client/RequestPluginsTest.php
+++ tests/Feature/Client/RequestPluginsTest.php
@@ -4,25 +4,17 @@ namespace Tests\Feature\Client;
 
 use Expose\Client\Configuration;
 use Expose\Client\Factory;
-use Expose\Client\Http\HttpClient;
 use Expose\Client\Logger\LoggedRequest;
 use Expose\Client\Logger\RequestLogger;
 use GuzzleHttp\Psr7\Message;
 use GuzzleHttp\Psr7\Request;
-use Illuminate\Support\Arr;
-use Mockery as m;
 use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use React\Http\Browser;
-use React\Http\Message\ResponseException;
 use Tests\Feature\TestCase;
 
 
 class RequestPluginsTest extends TestCase
 {
-
-//    use InteractsWithDashboard; // TODO: ?
-
     protected Browser $browser;
     protected Factory $dashboardFactory;
     protected RequestLogger $requestLogger;
@@ -56,7 +48,7 @@ class RequestPluginsTest extends TestCase
     /** @test */
     public function it_matches_a_paddle_request()
     {
-        $paddleRequest = new Request('GET', '/paddle-billing', $this->paddleHeader());
+        $paddleRequest = new Request('POST', '/paddle-billing', $this->paddleHeader());
 
         $this->logRequest($paddleRequest);
 
@@ -90,7 +82,7 @@ class RequestPluginsTest extends TestCase
         $this->assertArrayHasKey('plugin_data', $result[0]);
 
         $this->assertArrayHasKey('plugin', $result[0]['plugin_data']);
-        $this->assertEquals($result[0]['plugin_data'], [
+        $this->assertEquals([
             "plugin" => "Paddle Billing",
             "uiLabel" => "transaction.completed",
             "cliLabel" => "transaction.completed",
@@ -99,7 +91,7 @@ class RequestPluginsTest extends TestCase
                     'event_id' => '123456',
                     'notification_id' => '654321',
                 ]
-        ]);
+        ], $result[0]['plugin_data']);
     }
 
     /** @test */
@@ -117,23 +109,17 @@ class RequestPluginsTest extends TestCase
         $this->assertArrayHasKey(0, $result);
         $this->assertArrayHasKey('plugin_data', $result[0]);
 
-        $this->assertEquals($result[0]['plugin_data'], [
-            "plugin" => "Paddle Billing",
-            "uiLabel" => "plugin.error",
-            "cliLabel" => "plugin.error",
-            "details" => [
-                "Error" => "Trying to access array offset on null",
-                "File" => "/Users/dianascharf/Dev/expose-v3/app/Logger/Plugins/PaddleBillingPlugin.php", // TODO:
-                "Line" => 32,
-            ],
-        ]);
+        $this->assertEquals('plugin.error', $result[0]['plugin_data']['cliLabel']);
+        $this->assertEquals('plugin.error', $result[0]['plugin_data']['uiLabel']);
+        $this->assertArrayHasKey('details', $result[0]['plugin_data']);
+        $this->assertEquals('Trying to access array offset on null', $result[0]['plugin_data']['details']['Error']);
     }
 
 
     /** @test */
     public function it_matches_a_github_actions_request()
     {
-        $githubRequest = new Request('GET', '/github-actions', $this->githubActionsHeader());
+        $githubRequest = new Request('POST', '/github-actions', $this->githubActionsHeader());
 
         $this->logRequest($githubRequest);
 
@@ -148,6 +134,207 @@ class RequestPluginsTest extends TestCase
     }
 
     /** @test */
+    public function it_contains_github_push_data()
+    {
+        $githubData = [
+            "repository" => [
+                "full_name" => "beyondcode/expose"
+            ],
+            'ref' => 'refs/heads/main',
+            'pusher' => [
+                'name' => 'Diana Scharf',
+                'email' => 'diana@beyondco.de'
+            ],
+            'compare' => 'https://expose.dev',
+            'head_commit' => [
+                'message' => 'New features',
+                'added' => ['file1', 'file2'],
+                'removed' => ['file3'],
+                'modified' => ['file4']
+            ]
+        ];
+
+        $githubHeader = array_merge(
+            $this->githubActionsHeader(),
+            [
+                'x-github-event' => 'push',
+            ]
+        );
+
+        $githubRequest = new Request('POST', '/github-actions', $githubHeader, json_encode($githubData));
+
+        $this->logRequest($githubRequest);
+
+        $response = $this->await($this->browser->get('http://127.0.0.1:4040/api/logs'));
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertArrayHasKey(0, $result);
+        $this->assertArrayHasKey('plugin_data', $result[0]);
+
+        $this->assertArrayHasKey('plugin', $result[0]['plugin_data']);
+        $this->assertEquals([
+            "plugin" => "GitHub",
+            "uiLabel" => "push",
+            "cliLabel" => "push",
+            "details" => [
+                "Repository" => "beyondcode/expose",
+                "Branch" => "refs/heads/main",
+                "Author" => "Diana Scharf &lt;diana@beyondco.de&gt;",
+                "Compare" => "<a href='https://expose.dev'>https://expose.dev</a>",
+                "Commit" => "<span class='font-mono'>New features</span> <br/> (2 files added, 1 files removed, 1 files modified)"
+            ]
+        ], $result[0]['plugin_data']);
+    }
+
+    /** @test */
+    public function it_contains_github_issue_data()
+    {
+        $githubData = [
+            "repository" => [
+                "full_name" => "beyondcode/expose"
+            ],
+            'issue' => [
+                'number' => 42,
+                'title' => 'Feature request',
+                'user' => [
+                    'login' => 'mechelon'
+                ],
+                'html_url' => 'http://expose.dev'
+            ],
+        ];
+
+        $githubHeader = array_merge(
+            $this->githubActionsHeader(),
+            [
+                'x-github-event' => 'issues',
+            ]
+        );
+
+        $githubRequest = new Request('POST', '/github-actions', $githubHeader, json_encode($githubData));
+
+        $this->logRequest($githubRequest);
+
+        $response = $this->await($this->browser->get('http://127.0.0.1:4040/api/logs'));
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertArrayHasKey(0, $result);
+        $this->assertArrayHasKey('plugin_data', $result[0]);
+
+        $this->assertArrayHasKey('plugin', $result[0]['plugin_data']);
+        $this->assertEquals([
+            "plugin" => "GitHub",
+            "uiLabel" => "issues",
+            "cliLabel" => "issues",
+            "details" => [
+                "Repository" => "beyondcode/expose",
+                "Issue" => "#42 <span class='font-mono'>Feature request</span>",
+                "Author" => "mechelon",
+                "URL" => "<a href='http://expose.dev'>http://expose.dev</a>",
+            ]
+        ], $result[0]['plugin_data']);
+    }
+
+    /** @test */
+    public function it_contains_github_pull_request_data()
+    {
+        $githubData = [
+            "repository" => [
+                "full_name" => "beyondcode/expose"
+            ],
+            'pull_request' => [
+                'number' => 42,
+                'title' => 'Feature request',
+                'head' => [
+                    'ref' => 'refs/heads/new',
+                ],
+                'base' => [
+                    'ref' => 'refs/heads/main',
+                ],
+                'user' => [
+                    'login' => 'mechelon'
+                ],
+                'html_url' => 'http://expose.dev'
+            ],
+        ];
+
+        $githubHeader = array_merge(
+            $this->githubActionsHeader(),
+            [
+                'x-github-event' => 'pull_request',
+            ]
+        );
+
+        $githubRequest = new Request('POST', '/github-actions', $githubHeader, json_encode($githubData));
+
+        $this->logRequest($githubRequest);
+
+        $response = $this->await($this->browser->get('http://127.0.0.1:4040/api/logs'));
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertArrayHasKey(0, $result);
+        $this->assertArrayHasKey('plugin_data', $result[0]);
+
+        $this->assertArrayHasKey('plugin', $result[0]['plugin_data']);
+        $this->assertEquals([
+            "plugin" => "GitHub",
+            "uiLabel" => "pull_request",
+            "cliLabel" => "pull_request",
+            "details" => [
+                "Repository" => "beyondcode/expose",
+                "Pull Request" => "#42 <span class='font-mono'>Feature request</span>",
+                "Author" => "mechelon",
+                "Branch" => "<span class='font-mono'>refs/heads/new &rarr; refs/heads/main</span>",
+                "URL" => "<a href='http://expose.dev'>http://expose.dev</a>",
+            ]
+        ], $result[0]['plugin_data']);
+    }
+
+    /** @test */
+    public function it_contains_github_ping_data()
+    {
+        $githubData = [
+            "repository" => [
+                "full_name" => "beyondcode/expose"
+            ],
+            "hook_id" => 123,
+            'hook' => [
+                'name' => "Hook",
+                'events' => ['push', 'pull_request']
+            ],
+        ];
+
+        $githubHeader = array_merge(
+            $this->githubActionsHeader(),
+            [
+                'x-github-event' => 'ping',
+            ]
+        );
+
+        $githubRequest = new Request('POST', '/github-actions', $githubHeader, json_encode($githubData));
+
+        $this->logRequest($githubRequest);
+
+        $response = $this->await($this->browser->get('http://127.0.0.1:4040/api/logs'));
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertArrayHasKey(0, $result);
+        $this->assertArrayHasKey('plugin_data', $result[0]);
+
+        $this->assertArrayHasKey('plugin', $result[0]['plugin_data']);
+        $this->assertEquals([
+            "plugin" => "GitHub",
+            "uiLabel" => "ping",
+            "cliLabel" => "ping",
+            "details" => [
+                "Hook ID" => 123,
+                "Hook Name" => "Hook",
+                "Repository" => "beyondcode/expose",
+                "Hook Events" => "<span class='font-mono'>push, pull_request</span>"
+            ]
+        ], $result[0]['plugin_data']);
+    }
+
+    /** @test */
     public function it_returns_empty_plugin_data_for_unmatched_request()
     {
         $this->logRequest(new Request('GET', '/endpoint'));
@@ -159,8 +346,6 @@ class RequestPluginsTest extends TestCase
         $this->assertArrayHasKey('plugin_data', $result[0]);
         $this->assertEmpty($result[0]['plugin_data']);
     }
-
-    // TODO: GH actions
 
     protected function startDashboard()
     {
